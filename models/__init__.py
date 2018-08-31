110af0b1882f7a54854de8780c3466b14911e9b3
kayushka <machal.kaja@gmail.com>
2018-08-31 16:54:06 +0200

    zrobiona klasa users

--- models/__init__.py
+++ models/__init__.py
@@ -33,19 +33,64 @@ class Users:
             cursor.execute(sql, values)
             self.__id = cursor.fetchone()[0]
             return True
-        return False
+        else:
+            sql = """UPDATE Users SET username=%s, email=%s, hashed_password=%s
+                            WHERE id=%s"""
+            values = (self.username, self.email, self.hashed_password, self.id)
+            cursor.execute(sql, values)
+            return True
 
+    @staticmethod
+    def load_user_by_id(cursor, user_id):
+        sql = "SELECT id, username, email, hashed_password FROM Users WHERE id=%s"
+        cursor.execute(sql, (user_id,))
+        data = cursor.fetchone()
+        if data:
+            loaded_user = Users()
+            loaded_user.__id = data[0]
+            loaded_user.username = data[1]
+            loaded_user.email = data[2]
+            loaded_user.__hashed_password = data[3]
+            return loaded_user
+        else:
+            return None
+
+    @staticmethod
+    def load_all_users(cursor):
+        sql = "SELECT id, username, email, hashed_password FROM Users"
+        ret = []
+        cursor.execute(sql)
+        for row in cursor.fetchall():
+            loaded_user = Users()
+            loaded_user.__id = row[0]
+            loaded_user.username = row[1]
+            loaded_user.email = row[2]
+            loaded_user.__hashed_password = row[3]
+            ret.append(loaded_user)
+        return ret
+
+    def delete(self, cursor):
+        sql = "DELETE FROM Users WHERE id=%s"
+        cursor.execute(sql, (self.__id,))
+        self.__id = -1
+        return True
 
 if __name__ == '__main__':
-    izka = Users()
-    izka.username = "Izabela"
-    izka.email = "izuniak@sdfs.pl"
-    izka.set_password('pączuś', generate_salt())
-
     cnx = get_connection()
     cursor = cnx.cursor()
 
-    izka.save_to_db(cursor)
+    # s = (Users.load_all_users(cursor))
+    # b = []
+    # for _ in s:
+    #     b.append(_.username)
+    #
+    # print(b)
+
+    x = Users.load_user_by_id(cursor, 2)
+    x.delete(cursor)
+
+    # x.email = 'izka@hjkh.com'
+    # x.save_to_db(cursor)
 
     cursor.close()
     cnx.close()
